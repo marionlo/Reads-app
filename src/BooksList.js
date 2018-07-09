a3ec47650281af01915d486ded1f5e4e0ec2be6f
marion lotito <marion.lotito@gmail.com>
2018-07-09 18:38:06 +0000

    add book component on listbook

--- src/BooksList.js
+++ src/BooksList.js
@@ -17,7 +17,7 @@ class BooksList extends Component {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                     
+                     <Book />
                     </ol>
                   </div>
                 </div>
