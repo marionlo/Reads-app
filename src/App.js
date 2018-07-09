aff5ddae846260acb3fe53471a163e49482e4bed
Marion Lotito <marion.lotito@gmail.com>
2018-07-09 15:06:33 +0000

    feat: add componentdidmount for BooksAPI

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react'
 import BooksList from './BooksList'
 import SearchFilter from './SearchFilter'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+   books: []
+  }
 
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+    this.setState({books})
+  })
   }
 
   render() {
@@ -22,6 +22,7 @@ class BooksApp extends React.Component {
        
    		 <Route exact path="/" render={() =>  (
         <BooksList
+    books={this.state.books}
         />
       )}/>
 
