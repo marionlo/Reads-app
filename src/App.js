43e91d89fcc2aac20652aa5977118c1b914ce7b0
marion lotito <marion.lotito@gmail.com>
2018-07-09 18:37:03 +0000

    add book component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import BooksList from './BooksList'
+import Book from './Book'
 import SearchFilter from './SearchFilter'
 import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
@@ -13,16 +14,16 @@ class BooksApp extends React.Component {
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
     this.setState({books})
+console.log(books);
   })
   }
 
   render() {
+
     return (     
       <div className="app">
-       
    		 <Route exact path="/" render={() =>  (
-        <BooksList
-    books={this.state.books}
+        <BooksList books={this.state.books}
         />
       )}/>
 
